02d59d6a934ec7f9c8c67c6b12fc3653b4186754
Robert D. French <frenchrd@ornl.gov>
2014-04-28 12:56:09 -0400

    OMP print thread id

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <omp.h>
 #include <utmpx.h>
 
 int main(int argc, char **argv) {
@@ -7,8 +8,11 @@ int main(int argc, char **argv) {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	printf("Hello from rank %d\n", rank);
-
+	#pragma omp parallel
+	{
+		int thread_id = omp_get_thread_num();
+		printf("Hello from thread %d within rank %d\n", thread_id, rank);
+	}
 	MPI_Finalize();
 	return 0;
 }
