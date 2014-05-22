ce9983d036042bd2218e115e9547ff8534943d73
Robert D. French <frenchrd@ornl.gov>
2014-05-22 16:11:02 -0400

    hello world

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+hello_world.eos.exe: main.c
+	cc -fopenmp -o hello_world.eos.exe main.c
+	cp hello_world.eos.exe $(MEMBERWORK)/stf007
+
 hello_world.exe: main.c
 	mpicc -fopenmp -o hello_world.exe main.c
 	cp hello_world.exe $(MEMBERWORK)/stf007
@@ -5,3 +9,5 @@ hello_world.exe: main.c
 clean:
 	rm -f hello_world.exe
 	rm -f $(MEMBERWORK)/stf007/hello_world.exe
+	rm -f hello_world.eos.exe
+	rm -f $(MEMBERWORK)/stf007/hello_world.eos.exe
--- main.c
+++ main.c
@@ -2,16 +2,26 @@
 #include <mpi.h>
 #include <omp.h>
 #include <utmpx.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+	int num_megabytes = atoi(argv[1]);
+
+	char hostname[64];
+	gethostname(hostname,64);
+
+	if (rank == 0) printf("Host.cpu => rank.thread\n");
+	MPI_Barrier(MPI_COMM_WORLD);
+
 	#pragma omp parallel
 	{
 		int thread_id = omp_get_thread_num();
-		printf("Hello from thread %d within rank %d\n", thread_id, rank);
+		int cpu = sched_getcpu();
+		printf("%s.%d => %d.%d\n", hostname, cpu, rank, thread_id);
 	}
 	MPI_Finalize();
 	return 0;
