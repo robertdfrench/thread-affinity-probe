f53284821d481ff47ed6c195f45de5249eb2c979
Robert D. French <frenchrd@ornl.gov>
2014-05-30 09:27:26 -0400

    Add artificial workload and timing measurements

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
-hello_world.eos.exe: main.c
-	cc -fopenmp -o hello_world.eos.exe main.c
-	cp hello_world.eos.exe $(MEMBERWORK)/stf007
+thread-affinity-benchmark.titan.exe: main.c
+	cc -mp -o thread-affinity-benchmark.titan.exe main.c
 
-hello_world.exe: main.c
-	mpicc -fopenmp -o hello_world.exe main.c
-	cp hello_world.exe $(MEMBERWORK)/stf007
+thread-affinity-benchmark.eos.exe: main.c
+	cc -fopenmp -o thread-affinity-benchmark.eos.exe main.c
+	cp thread-affinity-benchmark.eos.exe $(MEMBERWORK)/stf007
+
+thread-affinity-benchmark.rhea.exe: main.c
+	mpicc -fopenmp -o thread-affinity-benchmark.rhea.exe main.c
+	cp thread-affinity-benchmark.rhea.exe $(MEMBERWORK)/stf007
 
 clean:
-	rm -f hello_world.exe
-	rm -f $(MEMBERWORK)/stf007/hello_world.exe
-	rm -f hello_world.eos.exe
-	rm -f $(MEMBERWORK)/stf007/hello_world.eos.exe
+	rm -f thread-affinity-benchmark.rhea.exe
+	rm -f thread-affinity-benchmark.eos.exe
+	rm -f thread-affinity-benchmark.titan.exe
+	rm -f $(MEMBERWORK)/stf007/thread-affinity-benchmark.rhea.exe
+	rm -f $(MEMBERWORK)/stf007/thread-affinity-benchmark.eos.exe
+	rm -f $(MEMBERWORK)/stf007/thread-affinity-benchmark.titan.exe
--- main.c
+++ main.c
@@ -3,26 +3,64 @@
 #include <omp.h>
 #include <utmpx.h>
 #include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
 
+#define length_of_hostname 64
+typedef struct thread_location {
+	char hostname[length_of_hostname];
+	int thread_id;
+	int cpu_id;
+	int rank;
+} ThreadLocation;
+
+void ThreadLocation_print(ThreadLocation tl) {
+	printf("\"hostname\": %s, \"cpu_id\": %d, \"rank\": %d, \"thread_id\": %d\n", tl.hostname, tl.cpu_id, tl.rank, tl.thread_id);
+}
+
+ThreadLocation ThreadLocation_create() {
+	ThreadLocation tl;
+	tl.thread_id = omp_get_thread_num();
+	tl.cpu_id = sched_getcpu();
+	MPI_Comm_rank(MPI_COMM_WORLD, &(tl.rank));
+	gethostname(tl.hostname, length_of_hostname);
+	return tl;
+}
+
+#define PI 3.1415
+void work_unit(double* data, int num_elements) {
+	int i;
+	double h = 1.0 / (double)num_elements;
+	#pragma omp parallel for
+	for(i=0; i < num_elements; i++) {
+		data[i] = sin(2*PI*i*h);
+	}
+}
+
+#define KB 1024
+#define MB 1024 * KB
 int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	int num_megabytes = atoi(argv[1]);
-
-	char hostname[64];
-	gethostname(hostname,64);
-
-	if (rank == 0) printf("Host.cpu => rank.thread\n");
-	MPI_Barrier(MPI_COMM_WORLD);
-
 	#pragma omp parallel
 	{
-		int thread_id = omp_get_thread_num();
-		int cpu = sched_getcpu();
-		printf("%s.%d => %d.%d\n", hostname, cpu, rank, thread_id);
+		ThreadLocation tl = ThreadLocation_create();
+		ThreadLocation_print(tl);
 	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	int data_size = 1200 * MB;
+	int num_elements = data_size / sizeof(double);
+	double* data = (double*)malloc(data_size);
+	double start_work = MPI_Wtime();
+	work_unit(data, num_elements);
+	double stop_work = MPI_Wtime();
+
+	printf("\"rank\": %d, \"time\": %f\n",rank,stop_work - start_work);
+
 	MPI_Finalize();
 	return 0;
 }
